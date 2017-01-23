b10236b9db83ab5c1d773e007ae7dec4480f0d40
Alexander Pustovalov <apustovalov@gmail.com>
2017-01-23 02:11:07 +0200

    Initial commit

---
+++ .structor/config.js
@@ -0,0 +1,10 @@
+/**
+ * Structor configuration
+ * This file will not be updated after the Structor's next installation
+ */
+
+module.exports = {
+  projectId: 179,
+  assetsRegexpPath: /app[\\\/]assets/,
+  srcPath: 'app',
+};
